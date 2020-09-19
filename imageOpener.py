421c2c245a1b8235a6c9709ae7e29dd4385d84de
Fimbulᚦulr <supergrazc@gmail>
2020-09-18 21:07:57 -0400

    imageOpener now functions by userscan

--- imageOpener.py
+++ imageOpener.py
@@ -1,7 +1,7 @@
 from PIL import Image
-
+filename = input("Enter your value: ")
 #read the image
-im1 = Image.open("e7239e3571035350c7286c67316258bc.jpg")
+im1 = Image.open(filename)
 
 #show images
 im1.show()
