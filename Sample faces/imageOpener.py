bf5d500821249d0f0dd64274e8e573ff361c0ef6
Fimbulᚦulr <supergrazc@gmail>
2020-09-18 21:23:52 -0400

    imageOpener now opens random file from faces
    This should work on everyone's computer. I am proud of this

--- Sample faces/imageOpener.py
+++ Sample faces/imageOpener.py
@@ -5,10 +5,7 @@ import os
 """
 filename = input("Enter your value: ")
 """
-"""
-dir = 'Users\super\Documents\GitHub\Machine-Learing-Nose-Jobs\Sample faces'
-"""
-filename = random.choice(os.listdir("C:/Users/super/Documents/GitHub/Machine-Learing-Nose-Jobs/Sample faces"))
+filename = random.choice(os.listdir(os.curdir))
 #read the image
 im1 = Image.open(filename)
 
