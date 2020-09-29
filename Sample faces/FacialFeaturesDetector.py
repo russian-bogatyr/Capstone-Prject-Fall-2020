7d82a5522d61b7fa45286c408ccce9c0afa3568f
Fimbulᚦulr <supergrazc@gmail>
2020-09-29 15:25:10 -0400

    This is ready for the app
    hooray

--- Sample faces/FacialFeaturesDetector.py
+++ Sample faces/FacialFeaturesDetector.py
@@ -3,15 +3,18 @@
 import cv2
 import numpy as np
 import dlib
+import random
+import os
 
 # Load the detector
 detector = dlib.get_frontal_face_detector()
 
+filename = random.choice(os.listdir(os.curdir))
 # Load the predictor
 predictor = dlib.shape_predictor(r"shape_predictor_68_face_landmarks.dat")
 
 # read the image
-img = cv2.imread(r"opencv_frame_0.png")
+img = cv2.imread(os.path.join(os.curdir, filename))
 
 # Convert image into grayscale
 gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
@@ -47,12 +50,12 @@ for face in faces:
 # show the image
 cv2.imshow(winname="Face", mat=img)
 # Delay between every fram
-#cv2.waitKey(delay=0)
+cv2.waitKey(delay=0)
 # Close all windows
 
 #cv2.destroyAllWindows()
-#print(empty_array)
-#empty_array.reshape(-1, 2)
+print(empty_array)
+empty_array.reshape(-1, 2)
 
 
 
