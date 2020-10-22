16d78a809337b756dcf6985c7225f43798f6369b
Fimbulᚦulr <supergrazc@gmail>
2020-10-22 14:57:05 -0400

    move realease version

--- release versions/version 0.0.1/version 0.0.2/test_call.py
+++
@@ -1,6 +0,0 @@
-import ratio_compute
-import pandas as pd
-data = ratio_compute.calculate_ratios()
-empty_arr = data[['Delta x','Delta y']].to_numpy()
-print(empty_arr)
-
---
+++ release versions/version 0.0.2/FacialFeatureClass.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  4 15:29:09 2020
+
+@author: Chris Graziano and also Tony did stuff
+"""
+
+import cv2
+import numpy as np
+import dlib
+
+class FacialFeatures():
+    def getCords(imagePath):
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor(r"shape_predictor_68_face_landmarks.dat")
+        #placeholder, should crate class for uploading picture
+        img = cv2.imread(imagePath)
+        gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+        # Use detector to find landmarks
+        facialPoints = np.array([])
+        faces = detector(gray)    
+        for face in faces:
+            # Create landmark object
+            landmarks = predictor(image=gray, box=face)
+            # Loop through all the points
+            for n in range(0, 68):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y        
+                facialPoints = np.append(facialPoints, ([x, y]) , axis = 0)
+                # Draw a circle
+                cv2.circle(img=img, center=(x, y), radius=3, color=(0,
+                                                                    255, 0), thickness=-1)
+                
+                #show the image
+                #cv2.imshow(winname=\"Face\", mat=img)
+                # Delay between every fram
+                #cv2.waitKey(delay=0)
+                # Close all windows
+                #cv2.destroyAllWindows()
+                
+
+        return np.reshape(facialPoints, (-1, 2))  
+        
