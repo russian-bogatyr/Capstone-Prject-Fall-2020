3ac46ca2b6ffdcedfde07a436bc04e773338ff38
Gatr13 <preseauj@my.erau.edu>
2020-11-05 15:04:25 -0500

    Fixed the shape predictor bug
    Fixed an issue where shape predictor could not be opened. RatioCompute no longer uses the chdir() function.

--- ratioCompute.py
+++ ratioCompute.py
@@ -10,10 +10,10 @@ import numpy as np
 
 #get_ipython().run_line_magic('matplotlib', 'notebook')
 def calculate_ratios():
-    #TO_DO 
+    #TO_DO
     #Change file path
-    os.chdir(os.path.join(os.path.dirname(os.curdir), 'csv_files'))
-    filepath = os.curdir
+    #os.chdir(os.path.join(os.path.dirname(os.curdir), 'csv_files'))
+    filepath = r"./csv_files"
     feature = pd.DataFrame(columns =["File","Delta x","Delta y","dy/dx"])
     #filepath = r"csv_files"
 
@@ -25,39 +25,39 @@ def calculate_ratios():
             if data.empty==False:
 
 
-            
+
                 #GOlDEN RATIO IS USED
                 dx = (data.loc[16]["x"]-data.loc[0]["x"])/(data.loc[16]["x"]-data.loc[42]["x"])
                 # Facial width to inner eye
-                
-                
+
+
                 dy = (data.loc[8]["y"]-data.loc[27]["y"])/(data.loc[8]["y"]-data.loc[64]["y"])
                 #Eyes to mouth to chin
-            
-                feature = feature.append({'File' :filename, 'Delta x' : dx, 'Delta y' : dy},  
-                        ignore_index = True) 
 
-         
+                feature = feature.append({'File' :filename, 'Delta x' : dx, 'Delta y' : dy},
+                        ignore_index = True)
+
+
 
     #remove plot
-                
+
     # y = feature["Delta y"].to_numpy()
     # x = feature["Delta x"].to_numpy()
     # plt.plot(x,y,"o",alpha=0.2)
     # plt.show()
     # #remove above
-    
+
     #feature.to_csv("golden_ratio.csv")
     return feature
 
-        
-        
+
+
 
 def calculate_ratio(facial_coordinates):
     empty_ar = np.array([])
-    
+
     x = (facial_coordinates[16][0] - facial_coordinates[0][0]) / (facial_coordinates[16][0] - facial_coordinates[42][0])
-    y = (facial_coordinates[8][1] - facial_coordinates[27][1]) / (facial_coordinates[8][1] - facial_coordinates[64][1]) 
+    y = (facial_coordinates[8][1] - facial_coordinates[27][1]) / (facial_coordinates[8][1] - facial_coordinates[64][1])
     final_ratio = np.reshape(empty_ar, (-1, 2))
     final_ratio = np.append(empty_ar, (x, y), axis = 0)
     #final_ratio = np.reshape(empty_ar, (-1, 2))
