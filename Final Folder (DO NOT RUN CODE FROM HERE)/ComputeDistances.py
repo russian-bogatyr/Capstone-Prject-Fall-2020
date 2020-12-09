6f6144a2664738ee35d18a27e4fdf9566d855588
Fimbulᚦulr <supergrazc@gmail>
2020-12-09 00:02:27 -0500

    Final Folder

---
+++ Final Folder (DO NOT RUN CODE FROM HERE)/ComputeDistances.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 17:02:55 2020
+
+@author: super & Jacob Preseau
+"""
+import numpy as np
+
+#nose points are 27-35
+def calcNoseRatio(facial_coordinates):
+    empty_ar = np.array([])
+    for i in range(27, 35):
+        x = facial_coordinates[i][0]
+        y = facial_coordinates[i][1]
+        noseArray = np.reshape(empty_ar, (-1, 2))
+        noseArray = np.append(empty_ar, (x, y), axis = 0)
+    return (noseArray)
+
+def euclideanDistance(test_point, neighbor_point):
+    # distance = 0.0
+    # for i in range(len(test_point)-1):
+    #     # dst = np.linalg.norm(test_point[i][1] - neighbor_point[i][1])
+    #     distance += (test_point[i, 0:2] - neighbor_point[i, 0:2])**2
+    return np.sqrt(np.sum((test_point - neighbor_point)**2))
+
+# Calculates the distances between normalized nose points
+def calcDifference(userArray, targetArray):
+  distances = []
+  targetNose = normalizeNosePoints(targetArray)
+  userNose = normalizeNosePoints(userArray)
+  for n in range(0,8):
+      distances.append(euclideanDistance(userNose[n], targetNose[n]))
+  return distances
+
+# Calculates the coordinate differences between nose points
+def calcDifferencesArray(userArray, targetArray):
+    differences = []
+    targetNose = normalizeNosePoints(targetArray)
+    userNose = normalizeNosePoints(userArray)
+
+    return targetRatio - userRatio
+
+# Normalizes and returns the entire face coordinate array.
+def normalizePoints(facial_coordinates):
+    # Use points 0 and 16 as the x interval, 24 and 8 as the y interval
+    minX = facial_coordinates[0][0]
+    maxX = facial_coordinates[16][0]
+    minY = facial_coordinates[24][1]
+    maxY = facial_coordinates[8][1]
+
+    center_point = facial_coordinates[30]
+
+    normalized_points = []
+
+    for i in range(len(facial_coordinates)):
+        newX = (facial_coordinates[i][0] - center_point[0]) / (maxX - minX)
+        newY = (facial_coordinates[i][1] - center_point[1]) / (maxY - minY)
+
+        normalized_points.append([newX, newY])
+
+    return normalized_points
+
+# Does the reverse of the normalizePoints function, returning a set of absolute points in pixels.
+def unNormalizePoints(normalized_points, minX, maxX, minY, maxY, center_point):
+
+    facial_coordinates = []
+
+    for i in range(len(normalized_points)):
+        newX = int(normalized_points[i][0] * (maxX - minX) + center_point[0])
+        newY = int(normalized_points[i][1] * (maxY - minY) + center_point[1])
+
+        facial_coordinates.append([newX, newY])
+
+    return facial_coordinates
+
+# Normalizes and returns just the nose points.
+def normalizeNosePoints(facial_coordinates):
+    # Use points 0 and 16 as the x interval, 24 and 8 as the y interval
+    minX = facial_coordinates[0][0]
+    maxX = facial_coordinates[16][0]
+    minY = facial_coordinates[24][1]
+    maxY = facial_coordinates[8][1]
+
+    normalized_points = []
+
+    for i in range(27, 35):
+        newX = (facial_coordinates[i][0] - minX) / (maxX - minX)
+        newY = (facial_coordinates[i][1] - minY) / (maxY - minY)
+
+        normalized_points.append([newX, newY])
+
+    return normalized_points
---
+++ Final Folder (DO NOT RUN CODE FROM HERE)/ratioCompute.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  4 15:29:09 2020
+
+@author: Chris Graziano and Tony
+"""
+import os
+import pandas as pd
+import numpy as np
+
+#get_ipython().run_line_magic('matplotlib', 'notebook')
+def calculate_ratios():
+    #TO_DO
+    #Change file path
+    #os.chdir(os.path.join(os.path.dirname(os.curdir), 'csv_files'))
+    filepath = r"./csv_files"
+    feature = pd.DataFrame(columns =["File","Delta x","Delta y","dy/dx"])
+    #filepath = r"csv_files"
+
+    for filename in os.listdir(filepath):
+        if ".csv" in filename:
+
+            data = pd.read_csv(os.path.join(filepath,filename))
+
+            if data.empty==False:
+
+
+
+                #GOlDEN RATIO IS USED
+                dx = (data.loc[16]["X"]-data.loc[0]["X"])/(data.loc[16]["X"]-data.loc[42]["X"])
+                # Facial width to inner eye
+
+
+                dy = (data.loc[8]["Y"]-data.loc[27]["Y"])/(data.loc[8]["Y"]-data.loc[64]["Y"])
+                #Eyes to mouth to chin
+
+                feature = feature.append({'File' :filename, 'Delta x' : dx, 'Delta y' : dy},
+                        ignore_index = True)
+
+
+
+    #remove plot
+
+    # y = feature["Delta y"].to_numpy()
+    # x = feature["Delta x"].to_numpy()
+    # plt.plot(x,y,"o",alpha=0.2)
+    # plt.show()
+    # #remove above
+
+    #feature.to_csv("golden_ratio.csv")
+    return feature
+
+
+
+
+def calculate_ratio(facial_coordinates):
+    empty_ar = np.array([])
+
+    x = (facial_coordinates[16][0] - facial_coordinates[0][0]) / (facial_coordinates[16][0] - facial_coordinates[42][0])
+    y = (facial_coordinates[8][1] - facial_coordinates[27][1]) / (facial_coordinates[8][1] - facial_coordinates[64][1])
+    final_ratio = np.reshape(empty_ar, (-1, 2))
+    final_ratio = np.append(empty_ar, (x, y), axis = 0)
+    #final_ratio = np.reshape(empty_ar, (-1, 2))
+    # remove plot
+    # plt.plot(x, y, 'r*')
+    return (final_ratio)
+calculate_ratios()
